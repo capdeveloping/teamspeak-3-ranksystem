02194cf940342609724859cbf555231bd524d311
captain <capdeveloping95@gmail.com>
2020-05-18 17:49:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ mkdir -p /var/www/html/$SUBFOLDER
 cd /tmp/gitfolder/
 git clone https://github.com/Newcomer1989/TSN-Ranksystem.git .
 cp -ur . /var/www/html/$SUBFOLDER
+rm -rf /tmp/gitfolder/
+
 chown -R www-data:www-data /var/www/html/$SUBFOLDER
 chmod -R ugo=rwx /var/www/html/$SUBFOLDER/update
 chmod -R ugo=rwx /var/www/html/$SUBFOLDER/logs
@@ -16,5 +18,9 @@ chmod ugo=rwx /var/www/html/$SUBFOLDER/other/dbconfig.php
 if ! "$SUBPATH" && ! grep -q "/var/www/html/$SUBFOLDER" /etc/apache2/sites-available/000-default.conf ; then
     sed -i ' s/\<var\/www\/html\>/&\/'$SUBFOLDER'/' /etc/apache2/sites-available/000-default.conf
 fi
+
+mkdir /var/www/html/ranksystem/logs/ -p
+chmod 740 /var/www/html/ranksystem/logs/
+chown -R www-data:www-data /var/www/html/ranksystem/logs/
 
 apache2-foreground
